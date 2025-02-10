d589ebb46a2427e609ee6a7ba49d6dfccae9297c
Hekije <hjep99@gmail.com>
2025-02-10 14:27:27 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+//setup... this is similar to when we use our default tags in html
+const express = require("express")
+//activate or tells this app variable to be an exppress server
+const app = express()
+const router = express.Router()
+
+//start the web server...app.listen(portnumber,function)
+app.listen(3000,function(){
+    console.log("Listening on port 3000")
+})
+
+//making an api uisn routes
+// Routes are used to handle browser requests. They look like URLs. 
+// The difference is that when a browser requests a route, it is dynamically handled by using a function. 
+
+//GET or a regular request when someone goes to http://localhost:3000/hello.
+//When using a function an a route, we almost always have a parameter or handle a response and request.
+app.get("/hello", function(req,res){
+    res.send("<h1>Hello Express</h1>")
+})
+app.get("/goodbye", function(req, res) {
+    res.send("<h1>Goodbye, Express!</h1>");
+});
